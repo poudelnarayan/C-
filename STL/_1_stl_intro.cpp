59e2759901de647e7c0d0f54e18b660e833422f0
Narayan Poudel <poudelnarayan434@gmail.com>
2022-05-25 21:55:17 +0545

    starting STL

---
+++ STL/_1_stl_intro.cpp
@@ -0,0 +1,34 @@
+/*
+A library of powerful, reusable , adaptable , generic classes and functions
+Implemented using C++ templates
+Implements common data structure and algorithm
+Huge class library!!
+Alexandar Stepanov(1994)
+
+*/
+
+/*
+Why Use STL?
+
+At its core STL is an assortment of commonly used container.
+The algorithms provided by the STL have published and well understood time and size complexity information.
+That means when you use the containers and algorithm in the STL  there are no surprises. You know exactly how the algorithm
+behave as the size of the collection increases.
+The STL has been aroud a long time . It's been tried and tested across many millions of lines of code.
+The STL is consistant , fast , type safe as wll as extensible.
+
+The STL has three main components:
+
+> Containers
+    . Collections of objects or primitive types.
+        (array,vector,deque,stack,set,map etc)
+
+
+> Algorithm
+    . functions for processing sequence of element from containers
+        (find, max, count , sort) etc
+
+> Iterators
+    . generate sequence of element from containers
+        (forward,reverse,by value, by refrence , constant etc)
+*/
